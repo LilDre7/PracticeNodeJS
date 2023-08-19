64858ba2729c8f78447fcf332631e2024ba3890d
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-08-18 23:31:56 -0600

    :memo: upadate document

---
+++ CrudDeUsuarios/src/controllers/users.controller.js
@@ -0,0 +1,7 @@
+const catchAsync = require("../utils/catchAsync");
+
+exports.createUser = catchAsync(async (req, res, next) => {});
+exports.getAllProduct = catchAsync(async (req, res, next) => {});
+exports.getUser = catchAsync(async (req, res, next) => {});
+exports.updateUser = catchAsync(async (req, res, next) => {});
+exports.deleteUser = catchAsync(async (req, res, next) => {});
