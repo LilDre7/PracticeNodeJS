81a6eaa9bce5cb8f4e1c1ee3926c3d002f4862cd
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-08-18 23:27:23 -0600

    :memo: upadate document

--- CrudDeUsuarios/src/app.js
+++ CrudDeUsuarios/src/app.js
@@ -1,10 +1,36 @@
+// Se requiere el paquete express y morgan para poder usarlo en el archivo principal
 const express = require("express");
-// const morgan = require("morgan");
+const morgan = require("morgan");
 
+// Se inicializa en una constante la variable app que es la que se exporta para usarla en el archivo principal
 const app = express();
 
 app.use(express.json());
 
-// app.use(morgan());
+// Morgan funciona para ver la peticiones de postman por consola
+app.use(morgan());
 
+// ** Routes ** //
+const userRoutes = require("./routes/userRoute");
+
+// ** Utilidades ** //
+const AppError = require("./utils/appError");
+
+// ** Middlewares de erroes ** //
+const globalErrorHandler = require("./controllers/errorController");
+
+// !! RUTAS FOR THE APPLICATION !! //
+app.use("/api/v1/users", userRoutes);
+
+app.use("*", (req, res, next) => {
+  new AppError(
+    `La ruta es incorrecta o no valida ${req.originalUrl} 🚑 `,
+    404,
+    "fail"
+  );
+});
+
+app.use(globalErrorHandler);
+
+// Se exporta el app para usarse en la conexion de la base de datos
 module.exports = app;
--- CrudDeUsuarios/src/routes/userRoute.js
+++ CrudDeUsuarios/src/routes/userRoute.js
@@ -1,3 +1,15 @@
 const express = require("express");
-
 const router = express.Router();
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+
+// ⚔️ Importacion para los contraladores ⚔️ //
+const userControl = require("../controllers/users.controller");
+
+router.post("/create", userControl.createUser);
+router.get("/allproduct", userControl.getAllProduct);
+router.get("/:id", userControl.getUser);
+router.put("/:id", userControl.updateUser);
+router.delete("/:id", userControl.deleteUser);
+
+module.exports = router;
